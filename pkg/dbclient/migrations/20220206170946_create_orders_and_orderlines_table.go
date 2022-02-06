e4c61ca529f6611a741887c8f076ed09522d1a04
sinanulker <sinanulker386@gmail.com>
2022-02-06 17:18:48 +0100

    Create orders and order_lines migration

---
+++ pkg/dbclient/migrations/20220206170946_create_orders_and_orderlines_table.go
@@ -0,0 +1,51 @@
+package migrations
+
+import (
+	"database/sql"
+	"github.com/pressly/goose/v3"
+)
+
+func init() {
+	goose.AddMigration(upCreateOrdersAndOrderlinesTable, downCreateOrdersAndOrderlinesTable)
+}
+
+func upCreateOrdersAndOrderlinesTable(tx *sql.Tx) error {
+	// This code is executed when the migration is applied.
+	_, err := tx.Exec(`CREATE TABLE orders (
+    						id bigserial primary key,
+    						created_at  timestamp without time zone DEFAULT now() NOT NULL,
+    						updated_at  timestamp without time zone DEFAULT now() NOT NULL, 
+    						customer varchar(256) not null
+						);`)
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec(`CREATE TABLE order_lines (
+    						id bigserial primary key,
+    						order_id bigint not null,
+    						created_at  timestamp without time zone DEFAULT now() NOT NULL,
+    						updated_at  timestamp without time zone DEFAULT now() NOT NULL, 
+    						customer varchar(256) not null,
+    						   CONSTRAINT fk_order
+									FOREIGN KEY(order_id) 
+									REFERENCES orders(id)
+									ON DELETE CASCADE
+						);`)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func downCreateOrdersAndOrderlinesTable(tx *sql.Tx) error {
+	// This code is executed when the migration is rolled back.
+	_, err := tx.Exec("DROP TABLE order_lines;")
+	if err != nil {
+		return err
+	}
+	_, err = tx.Exec("DROP TABLE orders;")
+	if err != nil {
+		return err
+	}
+	return nil
+}
